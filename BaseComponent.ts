fc3f23dc8615a0025846d4e8b2358ec6887a3703
aseplant <aseplantart@proton.me>
2025-07-29 18:58:06 -0700

    Add BaseComponent skeleton

---
+++ BaseComponent.ts
@@ -0,0 +1,10 @@
+type BaseComponentProps = {
+    id: String,
+    dataSource: String,
+    chartType: ChartType,
+    theme: String,
+    styling: String,
+}
+
+const BaseComponent = (props: BaseComponentProps) => {
+}
